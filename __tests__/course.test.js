5e2bb04e2d5b25d95351a5b99d323e46af0fa6fc
David Fekke <david@fekke.com>
2017-12-12 16:40:48 -0500

    Adding course correction function and test class

---
+++ __tests__/course.test.js
@@ -0,0 +1,22 @@
+
+const course = require('../lib/course');
+
+test('Find the correct wind vector for heading 60 and wind of 180', () => {
+    expect(course.windvector(60, 0)).toBe(120);
+});
+
+test('Find the correct wind vector for heading 30 and wind of 330', () => {
+    expect(course.windvector(30, 150)).toBe(60);
+});
+
+test('Find the correct wind vector for heading 150 and wind of 240', () => {
+    expect(course.windvector(150, 240)).toBe(90);
+});
+
+test('Find the correct wind vector for heading 300 and wind of 30', () => {
+    expect(course.windvector(300, 30)).toBe(90);
+});
+
+// test('Find the correct wind vector for heading 330 and wind of 30', () => {
+//     expect(course.windvector(330, 30)).toBe(60);
+// });
--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 
-function reverseCourse(degrees) {
-    if (degrees > 180) {
-        return degrees - 180;
-    } else {
-        return 180 + degrees;
-    }
-}
-
-console.log(223, reverseCourse(223));
-console.log(90, reverseCourse(90));
-console.log(30, reverseCourse(30));
-console.log(360, reverseCourse(360));
